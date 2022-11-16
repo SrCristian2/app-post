9638af25e23d4b0af4f0539680ed25beaa92e5dd
SrCristian2 <cristiancros12@gmail.com>
2022-11-16 18:56:17 -0500

    proyecto de aplicativo movil utilizando backend de imagenes hecho en clases anteriores

---
+++ src/components/Form/FormSubmitButton.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+export default function FormSubmitButton({ title, submitting, onPress }) {
+  const backgroundColor = "rgba(27,27,51,1)";
+
+  return (
+    <TouchableOpacity style={[styles.container, { backgroundColor }]}>
+      <Text style={{ fontSize: 18, color: "#fff" }} onPress={() => onPress()}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 45,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
